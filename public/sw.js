71e1a87c66b92a7294fbb1aadc0bcf428dd5f04f
max kolkema <mkolkema@gmail.com>
2026-09-15 01:51:20 -0500

    Refresh the PWA shell after updates

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,6 @@
 const CACHE = "tamatchi-v2";
 const CORE = ["/", "/manifest.webmanifest", "/icon.svg", "/animations/idle_blink.gif"];
 self.addEventListener("install", (event) => event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(CORE))));
+self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
+self.addEventListener("message", (event) => { if (event.data === "SKIP_WAITING") self.skipWaiting(); });
 self.addEventListener("fetch", (event) => event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request).then((response) => { const copy = response.clone(); caches.open(CACHE).then((cache) => cache.put(event.request, copy)); return response; }))));
